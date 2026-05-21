06cef60427e42bd94481a629288a405bb64bd824
Yujin1219 <jin-1219@naver.com>
2026-05-21 10:28:27 +0900

    Initial Commit

---
+++ Notio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notio
+//
+//  Created by Yujin on 5/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
